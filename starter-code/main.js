a603912995cefc39f4d229c2cb3c32525a76a96b
Adriana Black <adrianablack@adrianas-air.home>
2016-06-18 13:41:03 +0100

    final try at flipping cards to work

--- starter-code/main.js
+++ starter-code/main.js
@@ -15,7 +15,7 @@ var cards = ['queen', 'queen', 'king','king'];
 
 var cardsInPlay = [ ];
 
-var board=document.getElementById("game-board");
+var board = document.getElementById("game-board");
 
 // fuction to get game-board with cards
 function createBoard(){
@@ -24,29 +24,57 @@ function createBoard(){
 		//create new cards in js
 		var newCard = document.createElement('div');
 		newCard.className ='card';
+		//setting the card's 'data-card' to be the element of the array
 		newCard.setAttribute ('data-card', cards[i]);
 		board.appendChild(newCard);
-
+		//checking if card clicked is queen or king
+		//newCard.addEventListener ('click', kingOrQueen);
+		// when a card is clicked the function isTwoCards will be executed
 		newCard.addEventListener ('click', isTwoCards);
-		newCard.innerHTML = '<img src= "kingofclubs.png" alt ="King of Clubs">;'
 	
-
 	}
-};
+}
 
+/*var kingOrQueen = function {
+	if (this.getAttribute('data-card'))=== ('queen') {
+		newCard.innerHTML ='< img src= "queenofhearts.png" alt= "Queen of Hearts">'
+	}
+
+	else {
+		newCard.innerHTML = '<img src= "kingofclubs.png" alt ="King of Clubs">'
+	}
+};*/
+//checking if there are two cards in play
 var isTwoCards = function() {
-	cardsInPalay.push(this.getAttribute('data-card'));
 
-	if (cardsInPlay.length === 2){
-		isMatch (cardsInPlay);
-		// clear cards
-		cardsInPlay = [ ];
+	cardsInPlay.push(this.getAttribute('data-card'));
+	//console.log(this.getAttribute ('data-card'));
+
+		if (this.getAttribute('data-card')) === ('queen') {
+		this.innerHTML ='< img src= "queenofhearts.png" alt= "Queen of Hearts">'
+	}
+
+	else {
+		this.innerHTML = '<img src= "kingofclubs.png" alt ="King of Clubs">'
+	};
+
+			if (cardsInPlay.length === 2){
+				isMatch (cardsInPlay);
+		
+				// clear cards
+				cardsInPlay = [ ];
 	}
 }
-
+//function to check if two cards in play match
 var isMatch = function(){
+	if (cardsInPlay[0].getAttribute('data-card')) ===  (cardsInPlay[1].getAttribute('data-card')){
+	alert('You found a match!');
+	}
+else {
+	alert('sorry try again');
+	}
 
-}
+};
 createBoard ();
 /*for( var i=1; i <=4; i++){
 	var newCard = document.createElement("div");

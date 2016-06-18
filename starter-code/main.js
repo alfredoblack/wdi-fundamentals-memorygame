2f3dc7e2f5cf0728faf246f7d51da93701f70d03
Adriana Black <adrianablack@adrianas-air.home>
2016-06-18 17:39:53 +0100

    submit take three

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,12 +3,12 @@ var cardTwo ="queen";
 var cardThree ="king";
 var cardFour ="king";
 
-if (cardTwo === cardFour && cardOne === cardThree){
+/*if (cardTwo === cardFour && cardOne === cardThree){
 	alert('Sorry, try again');
 }
 else if (cardOne === cardTwo && cardThree === cardFour){
 	alert('You found a match!');
-}
+}*/
 
 
 var cards = ['queen', 'queen', 'king','king'];
@@ -20,7 +20,7 @@ var board = document.getElementById("game-board");
 // fuction to get game-board with cards
 function createBoard(){
 
-	for(var i=1; i <= cards.length; i++){
+	for(var i=0; i < cards.length; i++){
 		//create new cards in js
 		var newCard = document.createElement('div');
 		newCard.className ='card';
@@ -35,23 +35,14 @@ function createBoard(){
 	}
 }
 
-/*var kingOrQueen = function {
-	if (this.getAttribute('data-card'))=== ('queen') {
-		newCard.innerHTML ='< img src= "queenofhearts.png" alt= "Queen of Hearts">'
-	}
-
-	else {
-		newCard.innerHTML = '<img src= "kingofclubs.png" alt ="King of Clubs">'
-	}
-};*/
 //checking if there are two cards in play
 var isTwoCards = function() {
 
 	cardsInPlay.push(this.getAttribute('data-card'));
-	//console.log(this.getAttribute ('data-card'));
+	console.log(this.getAttribute ('data-card'));
 
-		if (this.getAttribute('data-card')) === ('queen') {
-		this.innerHTML ='< img src= "queenofhearts.png" alt= "Queen of Hearts">'
+		if (this.getAttribute('data-card') === 'queen') {
+		this.innerHTML ='<img src="queenofhearts.png" alt= "Queen of Hearts">'
 	}
 
 	else {
@@ -66,13 +57,14 @@ var isTwoCards = function() {
 	}
 }
 //function to check if two cards in play match
+
 var isMatch = function(){
-	if (cardsInPlay[0].getAttribute('data-card')) ===  (cardsInPlay[1].getAttribute('data-card')){
-	alert('You found a match!');
-	}
+    if (cardsInPlay[0].getAttribute('data-card') ===  cardsInPlay[1].getAttribute('data-card')){
+    alert('You found a match!');
+    }
 else {
-	alert('sorry try again');
-	}
+    alert('sorry try again');
+    }
 
 };
 createBoard ();
@@ -83,4 +75,13 @@ createBoard ();
 	console.log(i);
 }*/
 
+/*var kingOrQueen = function {
+	if (this.getAttribute('data-card'))=== ('queen') {
+		newCard.innerHTML ='<img src= "queenofhearts.png" alt= "Queen of Hearts">'
+	}
+
+	else {
+		newCard.innerHTML = '<img src= "kingofclubs.png" alt ="King of Clubs">'
+	}
+};*/
 
